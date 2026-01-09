4d8b9cf4e1d9db8a7d92323c5d605ef5164aac10
filipecheverrya <filipecheverrya@gmail.com>
2026-01-09 19:43:05 -0300

    fix: debug

--- services/fake-api.ts
+++ services/fake-api.ts
@@ -1,14 +1,14 @@
 import productsMock from '@/mock/products.json'
-const BASE = process.env.NEXT_PUBLIC_VERCEL_URL
+// const BASE = process.env.NEXT_PUBLIC_VERCEL_URL
 
 export async function getAllProducts(): Promise<ListProductsType | undefined> {
   try {
-    const response = await fetch(`${BASE}/products`, { cache: 'no-store' })
-    if (!response.ok) {
-      return productsMock
-      // throw new Error('Failed to fetch get_all_products')
-    }
-    return response.json()
+    return productsMock
+    // const response = await fetch(`${BASE}/products`, { cache: 'no-store' })
+    // if (!response.ok) {
+    // throw new Error('Failed to fetch get_all_products')
+    // }
+    // return response.json()
   } catch (err) {
     console.log('Failed to fetch get_all_products', err)
   }
@@ -16,12 +16,12 @@ export async function getAllProducts(): Promise<ListProductsType | undefined> {
 
 export async function getProductById({ id }: { id: string | string[] | undefined }): Promise<ProductType | undefined> {
   try {
-    const response = await fetch(`${BASE}/products/${id}`, { cache: 'no-store' })
-    if (!response.ok) {
-      return productsMock.filter(item => String(item.id) === id)[0]
-      // throw new Error('Failed to fetch get_product_by_id')
-    }
-    return response.json()
+    return productsMock.filter(item => String(item.id) === id)[0]
+    // const response = await fetch(`${BASE}/products/${id}`, { cache: 'no-store' })
+    // if (!response.ok) {
+    // throw new Error('Failed to fetch get_product_by_id')
+    // }
+    // return response.json()
   } catch (err) {
     console.log('Failed to fetch get_product_by_id', err)
   }
